3e435c2f8a04e463eb3fd8ea3143eb15cadb3d29
SIGDOS <sigdos2024@gmail.com>
2024-06-10 18:15:01 -0400

    Login

---
+++ hospital.sql
@@ -0,0 +1,376 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+CREATE TABLE `bitacora` (
+  `id` int(4) NOT NULL,
+  `name_user` varchar(20) CHARACTER SET utf8 COLLATE utf8_spanish2_ci NOT NULL,
+  `fecha_e` varchar(20) CHARACTER SET utf8 COLLATE utf8_spanish2_ci NOT NULL,
+  `fecha_s` varchar(20) CHARACTER SET utf8 COLLATE utf8_spanish2_ci NOT NULL,
+  `accion` varchar(80) CHARACTER SET utf8 COLLATE utf8_spanish2_ci NOT NULL,
+  `fecha_m` varchar(30) CHARACTER SET utf8 COLLATE utf8_spanish2_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cargo`
+--
+
+CREATE TABLE `cargo` (
+  `id` int(4) NOT NULL,
+  `cargo` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `departamento`
+--
+
+CREATE TABLE `departamento` (
+  `id` int(9) NOT NULL,
+  `codigo` varchar(25) NOT NULL,
+  `ala` varchar(25) NOT NULL,
+  `nombre_dpt` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `equipo`
+--
+
+CREATE TABLE `equipo` (
+  `id` int(5) NOT NULL,
+  `identificacion` varchar(25) NOT NULL,
+  `id_equipo` int(2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `equipo_departamento`
+--
+
+CREATE TABLE `equipo_departamento` (
+  `id` int(7) NOT NULL,
+  `id_equipo` int(4) NOT NULL,
+  `id_dpto` int(4) NOT NULL,
+  `id_tip_equipo` int(4) NOT NULL,
+  `id_loc_hospital` int(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `hospital_loc`
+--
+
+CREATE TABLE `hospital_loc` (
+  `id` int(5) NOT NULL,
+  `nombre_hosp` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `matriz_incidencia`
+--
+
+CREATE TABLE `matriz_incidencia` (
+  `id` int(7) NOT NULL,
+  `fecha_act` varchar(12) NOT NULL,
+  `caso_desc` varchar(40) NOT NULL,
+  `caso_cant` int(2) NOT NULL,
+  `nro_semana_ac` int(2) NOT NULL,
+  `nombr_hospital` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `orden`
+--
+
+CREATE TABLE `orden` (
+  `id` int(5) NOT NULL,
+  `num_orden` varchar(7) NOT NULL,
+  `fecha_creacion` varchar(18) NOT NULL,
+  `departamento` int(4) NOT NULL,
+  `tipo_servicio` int(4) NOT NULL,
+  `duracion` varchar(18) NOT NULL,
+  `fecha_cierre` varchar(18) NOT NULL,
+  `responsable` int(2) NOT NULL,
+  `estado` int(2) NOT NULL,
+  `observacion` varchar(255) NOT NULL,
+  `recursos` varchar(20) NOT NULL,
+  `tecnico` int(4) NOT NULL,
+  `id_equip` int(4) NOT NULL,
+  `mod_servicio` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `roles`
+--
+
+CREATE TABLE `roles` (
+  `id` int(4) NOT NULL,
+  `rol` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `servicio`
+--
+
+CREATE TABLE `servicio` (
+  `id` int(4) NOT NULL,
+  `nombre_servicio` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `status`
+--
+
+CREATE TABLE `status` (
+  `id` int(4) NOT NULL,
+  `orden_estado` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tipo_equipos`
+--
+
+CREATE TABLE `tipo_equipos` (
+  `id` int(4) NOT NULL,
+  `descripcion` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(4) NOT NULL,
+  `name` varchar(15) NOT NULL,
+  `lastname` varchar(15) NOT NULL,
+  `email` varchar(40) NOT NULL,
+  `password` varchar(15) NOT NULL,
+  `date` varchar(10) NOT NULL,
+  `tlf` int(12) NOT NULL,
+  `id_rol` int(2) NOT NULL,
+  `id_cargo` int(2) NOT NULL,
+  `departamento` int(4) NOT NULL,
+  `pregunta_segura` varchar(30) NOT NULL,
+  `id_hospital_loc` int(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `cargo`
+--
+ALTER TABLE `cargo`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id_cargo` (`cargo`);
+
+--
+-- Indices de la tabla `departamento`
+--
+ALTER TABLE `departamento`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `equipo`
+--
+ALTER TABLE `equipo`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id_equipo` (`id_equipo`);
+
+--
+-- Indices de la tabla `equipo_departamento`
+--
+ALTER TABLE `equipo_departamento`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id_equipo` (`id_equipo`),
+  ADD UNIQUE KEY `id_dpto` (`id_dpto`),
+  ADD UNIQUE KEY `id_tip_equipo` (`id_tip_equipo`),
+  ADD UNIQUE KEY `id_loc_hospital` (`id_loc_hospital`);
+
+--
+-- Indices de la tabla `hospital_loc`
+--
+ALTER TABLE `hospital_loc`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `matriz_incidencia`
+--
+ALTER TABLE `matriz_incidencia`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `orden`
+--
+ALTER TABLE `orden`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `responsable` (`responsable`),
+  ADD UNIQUE KEY `estado` (`estado`),
+  ADD UNIQUE KEY `tipo_servicio` (`tipo_servicio`),
+  ADD UNIQUE KEY `tecnico` (`tecnico`),
+  ADD UNIQUE KEY `departamento` (`departamento`),
+  ADD UNIQUE KEY `number` (`id_equip`);
+
+--
+-- Indices de la tabla `roles`
+--
+ALTER TABLE `roles`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `servicio`
+--
+ALTER TABLE `servicio`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `status`
+--
+ALTER TABLE `status`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `tipo_equipos`
+--
+ALTER TABLE `tipo_equipos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `rol` (`id_rol`),
+  ADD UNIQUE KEY `cargo` (`id_cargo`),
+  ADD UNIQUE KEY `id_hospital_loc` (`id_hospital_loc`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `cargo`
+--
+ALTER TABLE `cargo`
+  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `departamento`
+--
+ALTER TABLE `departamento`
+  MODIFY `id` int(9) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `equipo`
+--
+ALTER TABLE `equipo`
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `equipo_departamento`
+--
+ALTER TABLE `equipo_departamento`
+  MODIFY `id` int(7) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `matriz_incidencia`
+--
+ALTER TABLE `matriz_incidencia`
+  MODIFY `id` int(7) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `orden`
+--
+ALTER TABLE `orden`
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `servicio`
+--
+ALTER TABLE `servicio`
+  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `status`
+--
+ALTER TABLE `status`
+  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `tipo_equipos`
+--
+ALTER TABLE `tipo_equipos`
+  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `cargo`
+--
+ALTER TABLE `cargo`
+  ADD CONSTRAINT `cargo_ibfk_1` FOREIGN KEY (`id`) REFERENCES `user` (`id_cargo`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `equipo`
+--
+ALTER TABLE `equipo`
+  ADD CONSTRAINT `equipo_ibfk_1` FOREIGN KEY (`id_equipo`) REFERENCES `tipo_equipos` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `equipo_departamento`
+--
+ALTER TABLE `equipo_departamento`
+  ADD CONSTRAINT `equipo_departamento_ibfk_1` FOREIGN KEY (`id_equipo`) REFERENCES `equipo` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `equipo_departamento_ibfk_2` FOREIGN KEY (`id_tip_equipo`) REFERENCES `equipo` (`id_equipo`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `equipo_departamento_ibfk_3` FOREIGN KEY (`id_dpto`) REFERENCES `departamento` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `equipo_departamento_ibfk_4` FOREIGN KEY (`id_loc_hospital`) REFERENCES `hospital_loc` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `orden`
+--
+ALTER TABLE `orden`
+  ADD CONSTRAINT `orden_ibfk_1` FOREIGN KEY (`estado`) REFERENCES `status` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `orden_ibfk_3` FOREIGN KEY (`tipo_servicio`) REFERENCES `servicio` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `orden_ibfk_4` FOREIGN KEY (`responsable`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `orden_ibfk_5` FOREIGN KEY (`tecnico`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `orden_ibfk_6` FOREIGN KEY (`departamento`) REFERENCES `departamento` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `orden_ibfk_7` FOREIGN KEY (`id_equip`) REFERENCES `equipo` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `user`
+--
+ALTER TABLE `user`
+  ADD CONSTRAINT `user_ibfk_1` FOREIGN KEY (`id_rol`) REFERENCES `roles` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `user_ibfk_2` FOREIGN KEY (`id_hospital_loc`) REFERENCES `hospital_loc` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
