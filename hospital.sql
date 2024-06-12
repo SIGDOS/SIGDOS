3a5b05d1d48d1796adf46c73dc7314ca4163b41e
SIGDOS <sigdos2024@gmail.com>
2024-06-12 13:21:17 -0400

    Update hospital.sql

--- hospital.sql
+++ hospital.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 12-06-2024 a las 17:48:04
+-- Tiempo de generación: 12-06-2024 a las 19:20:16
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
